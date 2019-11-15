4a4010729ebc95ba7c33850734aef0a48b689c84
im-cy <44048665+im-cy@users.noreply.github.com>
2019-11-16 04:12:13 +0800

    ?

--- rss_fetch/main.py
+++ rss_fetch/main.py
@@ -1,14 +1,52 @@
 import feedparser
 import json
+from urllib import request
+MESSES_NUMBER = 3
+
+
+def check_url(url):
+    with request.urlopen(url) as file:
+        print(file.status)
+        print(file.reason)
+        ans = file.reason
+    return ans
+
 
 fin = open('rss_list.json', encoding='utf-8')
 content = fin.read()
-jsonreader = json.loads(content)
-for List in jsonreader:
+json_reader = json.loads(content)
+for List in json_reader:
+    last_mess = {}
     for j in List:
         if j == 'url':
-            rssreader = feedparser.parse(List[j])
-            print(rssreader.keys())
-            for key in rssreader.keys():
-                print(rssreader[key])
-#print(jsonreader)
+            rss_reader = feedparser.parse(List[j])
+            print(rss_reader.keys())
+            #           print(rss_reader['entries'])
+            texts = []
+            for index, lis in enumerate(rss_reader['entries']):
+                if index >= MESSES_NUMBER:
+                    break
+                text = ''
+                text = lis['title'] + '$' + lis['link'] + '$' + lis['published'] + '$' + lis['summary'] + '$' + lis['author']
+                if text == last_mess[List[j]]:
+                    break
+                texts.append(text)
+            last_mess[List[j]] = texts[0]
+#                print(lis['content'][0].keys())
+
+
+
+#qq_group_id, qq_id_list, text, img
+
+
+
+
+#print(lis.keys())
+#print(lis['title'])  # 标题
+#print(lis['link'])  # 链接
+#print(lis['published'])  # 发布时间
+#                print(lis['updated'])#更新时间
+#print(lis['summary'])  # 简介
+#                print(lis['authors'])#作者们的资料
+#                print(lis['href'])#形似主页
+#print(lis['author'])  # 作者？主作者之类的
