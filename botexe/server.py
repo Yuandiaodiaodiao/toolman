f7a7c5fc7c65157f6294797d4896c764850d1814
Yuan <33371662+Yuan-ZH@users.noreply.github.com>
2019-11-16 01:51:04 +0800

    add rss plugin framework

--- botexe/server.py
+++ botexe/server.py
@@ -1,6 +1,12 @@
 import nonebot
+import config
+from os import path
 
 if __name__ == "__main__":
-    nonebot.init()
-    nonebot.load_builtin_plugins()
+    nonebot.init(config)
+    # nonebot.load_builtin_plugins()
+    nonebot.load_plugins(
+        path.join(path.dirname(__file__), 'plugins'),
+        'plugins'
+    )
     nonebot.run(host='127.0.0.1', port=8080)
