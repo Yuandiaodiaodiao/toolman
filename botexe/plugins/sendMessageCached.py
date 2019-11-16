9cf04ebc09dea4b4e81775a20437bb4f333354ef
Yuan <33371662+Yuan-ZH@users.noreply.github.com>
2019-11-16 20:00:55 +0800

    add requirements

--- botexe/plugins/sendMessageCached.py
+++ botexe/plugins/sendMessageCached.py
@@ -4,34 +4,39 @@ import nonebot
 import pytz
 from aiocqhttp.exceptions import Error as CQHttpError
 import json
-from nonebot.message import MessageSegment,Message
-@nonebot.scheduler.scheduled_job('interval',seconds=5)
+from nonebot.message import MessageSegment, Message
+
+
+@nonebot.scheduler.scheduled_job('interval', seconds=1)
 async def _():
     bot = nonebot.get_bot()
 
     try:
         pass
-        data={
-            'formBot':True
+        data = {
+            'formBot': True
         }
         try:
-            res=requests.post('http://127.0.0.1:50382',data=json.dumps(data))
-            js=json.loads(res.text)
+            res = requests.post('http://127.0.0.1:50382', data=json.dumps(data))
+            js = json.loads(res.text)
         except:
             return
         for i in js:
             print(i)
-            msg=Message('')
+            msg = Message('')
             # int(i.get('qq_group_id'))
             for j in i.get('qq_id_list'):
                 msg.append(MessageSegment.at(int(j)))
             msg.extend('\n')
-            textObj=i.get('text')
-            if isinstance(textObj,list):
+            textObj = i.get('text')
+            if isinstance(textObj, list):
                 for j in textObj:
-                    msg.extend(j.replace('$',' '))
+                    msg.extend(j.replace('$', ' '))
             else:
-                msg.extend(i.get('text'))
+                try:
+                    msg.extend(i.get('text'))
+                except:
+                    print(f'i.get() error')
             await bot.send_group_msg(group_id=967636480,
                                      message=msg)
             print('say')
