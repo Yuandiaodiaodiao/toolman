7305a107db504f600629251ed0caccf9e4a6ccc5
Yuan <33371662+Yuan-ZH@users.noreply.github.com>
2019-11-17 07:05:24 +0800

    change dfa

--- botexe/plugins/filter.py
+++ botexe/plugins/filter.py
@@ -2,6 +2,7 @@ from nonebot import on_command, CommandSession
 from nonebot.typing import Context_T
 import nonebot.helpers
 import nonebot
+from dfaMaster.app.test import check_sensitive
 bot = nonebot.get_bot()
 @bot.on_message('group')
 async def handle_group_message(ctx: Context_T):
@@ -9,3 +10,13 @@ async def handle_group_message(ctx: Context_T):
     for i in ctx.get('message'):
         if i.type == 'text':
             text += i.data['text']
+
+    ifDangerous = check_sensitive(text)
+    try:
+      pass
+    except:
+        print('dfa error')
+        return
+    if ifDangerous:
+        text='危险言论'
+        await nonebot.helpers.send(bot=bot, ctx=ctx, message=text)
--- dfa-master/__init__.py
+++
@@ -1,4 +0,0 @@
-__author__ = "Black"
-__email__ = "cj194832@163.com"
-__version = "V1.0"
-
