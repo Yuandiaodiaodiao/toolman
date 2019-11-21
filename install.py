a66cc17a9fb8188564c0ba4edceea1c058c787f0
Yuan <33371662+Yuandiaodiaodiao@users.noreply.github.com>
2019-11-22 05:26:08 +0800

    Update install.py

--- install.py
+++ install.py
@@ -1,12 +1,14 @@
 import os
 import sys
 if __name__=="__main__":
-    pythonstr=sys.argv[1]
-    pipstr=sys.argv[2]
-    if len(pipstr)==0:
-        pipstr="pip"
-    if len(pythonstr)==0:
+    if len(sys.argv)>=2:
+        pythonstr=sys.argv[1]
+    else:
         pythonstr="python"
+    if len(sys.argv)>=3:
+        pipstr=sys.argv[2]
+    else:
+        pipstr="pip"
     os.system(f"{pipstr}   install -r requirements.txt")
     try:
         os.mkdir('./install')
