1cac4aa51c476119f80481f3800b109e5782d7e6
Yuan <33371662+Yuandiaodiaodiao@users.noreply.github.com>
2019-11-22 05:24:18 +0800

    Update install.py

--- install.py
+++ install.py
@@ -1,15 +1,22 @@
 import os
-
-os.system('pip install -r requirements.txt')
-try:
-    os.mkdir('./install')
-except:
-    pass
-try:
-    os.mkdir('./install/Abstract_Words')
-except:
-    pass
-os.system('git clone https://github.com/yhm12345/Abstract_Words.git ./install/Abstract_Words')
-os.chdir('./install/Abstract_Words')
-os.system('python setup.py sdist')
-os.system('python setup.py install')
+import sys
+if __name__=="__main__":
+    pythonstr=sys.argv[1]
+    pipstr=sys.argv[2]
+    if len(pipstr)==0:
+        pipstr="pip"
+    if len(pythonstr)==0:
+        pythonstr="python"
+    os.system(f"{pipstr}   install -r requirements.txt")
+    try:
+        os.mkdir('./install')
+    except:
+        pass
+    try:
+        os.mkdir('./install/Abstract_Words')
+    except:
+        pass
+    os.system('git clone https://github.com/yhm12345/Abstract_Words.git ./install/Abstract_Words')
+    os.chdir('./install/Abstract_Words')
+    os.system(f'{pythonstr} setup.py sdist')
+    os.system(f'{pythonstr} setup.py install')
