72c9f1a72eb4ba990a410e9cc0e11306ecdb7077
Yuan <33371662+Yuandiaodiaodiao@users.noreply.github.com>
2019-11-22 06:21:20 +0800

    Update install.py

--- install.py
+++ install.py
@@ -11,15 +11,20 @@ if __name__=="__main__":
         pipstr="pip"
     os.system(f"{pipstr}   install -r requirements.txt")
     os.system(f'{pipstr}   install "nonebot[scheduler]"')
+
+    # install ab
     try:
-        os.mkdir('./install')
-    except:
-        pass
-    try:
-        os.mkdir('./install/Abstract_Words')
-    except:
-        pass
-    os.system('git clone https://github.com/yhm12345/Abstract_Words.git ./install/Abstract_Words')
-    os.chdir('./install/Abstract_Words')
-    os.system(f'{pythonstr} setup.py sdist')
-    os.system(f'{pythonstr} setup.py install')
+        import ab
+    except ImportError:
+        try:
+            os.mkdir('./install')
+        except:
+            pass
+        try:
+            os.mkdir('./install/Abstract_Words')
+        except:
+            pass
+        os.system('git clone https://github.com/yhm12345/Abstract_Words.git ./install/Abstract_Words')
+        os.chdir('./install/Abstract_Words')
+        os.system(f'{pythonstr} setup.py sdist')
+        os.system(f'{pythonstr} setup.py install')
