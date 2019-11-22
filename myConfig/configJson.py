b53f9e6fe3e8f266f4fe30811048f4f9d899c3f6
Yuan <33371662+Yuandiaodiaodiao@users.noreply.github.com>
2019-11-23 03:27:24 +0800

    change json5

--- myConfig/configJson.py
+++ myConfig/configJson.py
@@ -1,7 +1,7 @@
 import json
 import os
-
+import json5
 PATH_THIS_FLODER = os.path.dirname(os.path.abspath(__file__))
 PATH_CONFIG = os.path.join(PATH_THIS_FLODER, 'config.json')
 with open(PATH_CONFIG, 'r')as f:
-    configJs = json.load(f)
+    configJs = json5.load(f)
