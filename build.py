522efac75c6f37ee516b4e7c9cfc76c027d5c24e
Yuan <33371662+Yuandiaodiaodiao@users.noreply.github.com>
2019-11-23 04:11:42 +0800

    finish

--- build.py
+++ build.py
@@ -6,9 +6,9 @@ if __name__=="__main__":
         import env_windows.launcher
         env_windows.launcher.run()
         if configJs["isPro"]:
-            os.system('./env_windows/CQP.exe')
+            os.system('"env_windows\\CQP.exe"')
         else:
-            os.system('./env_windows/CQA.exe')
+            os.system('"env_windows\\CQA.exe"')
     else:
         import env_linux.launcher
         env_linux.launcher.run('init')
