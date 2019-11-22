f3f17677591e918e0cf9e02adb3590bce179e750
suxb201 <sxb201@qq.com>
2019-11-23 01:22:12 +0800

    fix rss

--- rss_fetch/matprint.py
+++ rss_fetch/matprint.py
@@ -4,14 +4,12 @@ def mat_put(a):
     listl = [[0] * l for i in range(l)]
     for i in range(0, l):
         for j in range(0, l):
-            for k in range(0,n):
+            for k in range(0, n):
                 if i == k or i == l - k - 1 or j == k or j == l - k - 1:
                     listl[i][j] = a[k]
                     break
     return listl
-    for i in range(0,l):
+    for i in range(0, l):
         print(listl[i])
-
-
 
 # mat_put([1, 2, 3])
--- run.py
+++ run.py
@@ -3,12 +3,13 @@ import botCore.botServer
 import botCore.messageServer
 import rss_fetch.main
 import messages.messages_server
-if __name__=="__main__":
-    processList=[]
-    processList.append(Process(target=botCore.botServer.run,name="botCore.botServer"))
-    processList.append(Process(target=botCore.messageServer.run,name="botCore.messageServer"))
-    processList.append(Process(target=rss_fetch.main.run,name="rss_fetch.main"))
-    processList.append(Process(target=messages.messages_server.run,name="messages.messages_server"))
+
+if __name__ == "__main__":
+    processList = []
+    processList.append(Process(target=botCore.botServer.run, name="botCore.botServer"))
+    processList.append(Process(target=botCore.messageServer.run, name="botCore.messageServer"))
+    processList.append(Process(target=rss_fetch.main.run, name="rss_fetch.main"))
+    processList.append(Process(target=messages.messages_server.run, name="messages.messages_server"))
     for i in processList:
         i.start()
     while True:
